1ce17e435e47d827a39de0a1b4898d7ee752defe
Peter.Peng <peter.peng@missionsky.com>
2017-03-25 11:21:25 +0800

    提交漏缺的文件

---
+++ DailyCaseHelper/Proxy/models/AccelaCaseStatusMapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.smartwork.Proxy.models
+{
+    public class AccelaCaseStatusMapper
+    {
+        public string SalesforceID { get; set; }
+        public string SuppJiraID { get; set; }
+        public string SuppAssignee { get; set; }
+        public string LinkedJiraID { get; set; }
+        public string LinkedJiraAssignee { get; set; }
+        public string Summary { get; set; }
+        public string SFQueue { get; set; }
+        public string SFStatus { get; set; }
+        public string SuppJiraStatus { get; set; }
+        public string LinkedJiraStatus { get; set; }
+        public bool NeedSync { get; set; }
+    }
+}
