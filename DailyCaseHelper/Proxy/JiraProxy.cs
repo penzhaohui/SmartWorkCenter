7b608f4a387d34b346d35d0e807f979780a22e2f
Peter.Peng <peter.peng@missionsky.com>
2017-07-13 12:53:53 +0800

    排除Task 和 Sub Task

--- DailyCaseHelper/Proxy/JiraProxy.cs
+++ DailyCaseHelper/Proxy/JiraProxy.cs
@@ -203,7 +203,7 @@ namespace com.smartwork.Proxy
         public static async Task<List<Issue>> GetIssueListByStatus(string status)
         {
             IJiraClient jira = new JiraClient("https://accelaeng.atlassian.net/", "peter.peng@missionsky.com", "peter.peng");
-            string sql = "project = ENGSUPP AND status = \"" + status + "\" AND reporter in (\"Peter.peng@missionsky.com\")";
+            string sql = "project = ENGSUPP AND  issuetype not in (Task, Sub-task) AND status = \"" + status + "\" AND reporter in (\"Peter.peng@missionsky.com\")";
 
             List<Issue> issueList = new List<Issue>();
             var issues = jira.GetIssuesByQuery("ENGSUPP", "", sql);
@@ -472,7 +472,7 @@ namespace com.smartwork.Proxy
             IJiraClient jira = new JiraClient("https://accelaeng.atlassian.net/", "peter.peng@missionsky.com", "peter.peng");
 
             // http://www.cnblogs.com/polk6/p/5465088.html
-            string sql = "reporter in (\"Peter.peng@missionsky.com\") AND created >= " + start.ToString("yyyy-MM-dd") + " AND created < " + end.ToString("yyyy-MM-dd") + " ";
+            string sql = " issuetype not in (Task, Sub-task) AND reporter in (\"Peter.peng@missionsky.com\") AND created >= " + start.ToString("yyyy-MM-dd") + " AND created < " + end.ToString("yyyy-MM-dd") + " ";
             
             List<Issue> issueList = new List<Issue>();
             var issues = jira.GetIssuesByQuery("ENGSUPP", "", sql);
@@ -489,7 +489,7 @@ namespace com.smartwork.Proxy
             IJiraClient jira = new JiraClient("https://accelaeng.atlassian.net/", "peter.peng@missionsky.com", "peter.peng");
 
             // http://www.cnblogs.com/polk6/p/5465088.html
-            string sql = " status in (Open, \"In Progress\", Reopened, Pending, \"Development in Progress\") AND not \"Salesforce ID\" is EMPTY AND reporter in (\"Peter.peng@missionsky.com\") ";
+            string sql = " issuetype not in (Task, Sub-task) AND status in (Open, \"In Progress\", Reopened, Pending, \"Development in Progress\") AND not \"Salesforce ID\" is EMPTY AND reporter in (\"Peter.peng@missionsky.com\") ";
 
             List<Issue> issueList = new List<Issue>();
             var issues = jira.GetIssuesByQuery("ENGSUPP", "Bug", sql);
